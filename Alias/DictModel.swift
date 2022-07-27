678d1777f46507b8c241986e908f9fad88df80a0
vladislavikys <49569336+vladislavikys@users.noreply.github.com>
2022-07-27 15:32:36 +0300

    add array dict

---
+++ Alias/DictModel.swift
@@ -0,0 +1,8 @@
+//
+//  DictModel.swift
+//  Alias
+//
+//  Created by vlad zarya on 27.07.22.
+//
+
+import Foundation
---
+++ GamesModel.swift
@@ -0,0 +1,8 @@
+//
+//  GamesModel.swift
+//  Alias
+//
+//  Created by vlad zarya on 26.07.22.
+//
+
+import Foundation

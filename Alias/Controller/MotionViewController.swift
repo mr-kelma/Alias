7f3754af8350c6c89ae4abd89428180c29c664ad
Markus163 <pprimak@mail.ru>
2022-07-31 19:12:27 +0400

    Add ActionBrain, write ActionModel, MotionView & Segues.

--- Alias/Controller/MotionViewController.swift
+++ Alias/Controller/MotionViewController.swift
@@ -10,6 +10,8 @@ import AVFoundation
 
 class MotionViewController: UIViewController {
 
+    
+
     @IBOutlet weak var nameTeam: UILabel!
     @IBOutlet weak var scoreTeam: UILabel!
     @IBOutlet weak var timer: UILabel!
@@ -17,11 +19,26 @@ class MotionViewController: UIViewController {
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var wrongButton: UIButton!
     
+    var actionBrain: ActionBrain = ActionBrain()
+    var gameBrain: GameBrain = GameBrain()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         startViews()
+        setupGame()
+        
+    }
+    
+    func setupGame() {
+        //Счет команды
+        scoreTeam.text = String(gameBrain.selectedTeam?.score ?? 0)
+        //Название команды
+        nameTeam.text = gameBrain.selectedTeam?.name
+        actionBrain.getRandomActionWord()
+        questionAction.text = actionBrain.currentWord
+        
     }
     
     func startViews() {
@@ -31,10 +48,14 @@ class MotionViewController: UIViewController {
     
     }
     @IBAction func pressedRightButton(_ sender: UIButton) {
+        actionBrain.addPoint()
         playSound(resource: "ПРАВИЛЬНО")
+        self.performSegue(withIdentifier: "backToGame", sender: self)
     }
     
     @IBAction func pressedWrongButton(_ sender: UIButton) {
+        actionBrain.minusPoint()
         playSound(resource: "ПРОПУСТИТЬ")
+        self.performSegue(withIdentifier: "backToGame2", sender: self)
     }
 }
--- Alias/Model/ActionModel.swift
+++ Alias/Model/ActionModel.swift
@@ -4,12 +4,21 @@
 //
 //  Created by Марк Михайлов on 29.07.2022.
 //
-
+    
 import Foundation
-    
-    
 
+
+struct ActionWordCategory {
+    let words: [String]
     
+    init(words: [String]) {
+       self.words = words
+    }
+
+    static let actionWord = ActionWordCategory (words: ["Изобрази грусть😞", "Изобрази гнев😠", "Изобрази плач😭", "Изобрази, что ты заболел", "Изобрази игру на барабанах", "Изобрази капитана корабля", "Изобрази обезъяну", "Изображи гаишника", "Изобрази игру на гитаре", "Изобрази начальника"])
+    }
+     
+
     
 
 

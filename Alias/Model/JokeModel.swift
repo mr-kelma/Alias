b9d88b81ad9e7bd73755934bc1698c9bed769c6b
Markus163 <pprimak@mail.ru>
2022-08-01 19:00:09 +0400

    JSON no final

---
+++ Alias/Model/JokeModel.swift
@@ -0,0 +1,16 @@
+//
+//  JokeModel.swift
+//  Alias
+//
+//  Created by Марк Михайлов on 01.08.2022.
+//
+
+import Foundation
+
+struct JokeModel {
+    let setup: String
+    let punchline: String
+}
+    
+    
+
